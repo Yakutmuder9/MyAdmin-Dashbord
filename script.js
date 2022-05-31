c4f622c41d2697d88fbceec4eedcaf4675362e82
Yakutmuder9 <yakutmuder9@gmail.com>
2022-05-30 21:47:34 -0400

    sd

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const navbarTop = document.getElementById("navbar_top")
 const sideToggler = document.getElementById("side_toggler")
+const navNavMain = document.getElementById("sidenav-main")
 const navLinkText = document.querySelector("navlikn_text")
 
+document.addEventListener("DOMContentLoaded", function(){
   window.addEventListener('scroll', function() {
-      if (window.scrollY > 20) {
-        navbarTop.classList.add('bg-dark');
-          console.log("scroll20 and more")
+      if (window.scrollY > 50) {
+        navbarTop.classList.add('fixed-top');
+        document.body.style.background = 'black';
       } else {
-        navbarTop.classList.remove('bg-dark');
-        console.log("scroll20 and less")
+        navbarTop.classList.remove('fixed-top');
+        document.body.style.background = 'black';
       } 
   });
+}); 
+
 
 sideToggler.addEventListener('click', function() {
   document.getElementById('sidenav-main').style.width='5vw';
-  document.getElementById('navlikn_text').style.display="none!important";
+  navLinkText.style.display="none!important";
+  navNavMain.style.display="none!important";
 })
